5c2e28febef3ce2fd8ab83f3b7b4ac00f1607821
user202729 <25191436+user202729@users.noreply.github.com>
2019-02-26 17:35:23 +0700

    WIP: multi-mode

--- src/main.cpp
+++ src/main.cpp
@@ -19,31 +19,170 @@ Grid grid;
 
 char const* const window_name="lcdreader-img";
 char const* const outwin_name="output";
-int const zoom_factor=15;
+char const* const histwin_name="history";
+int const zoom_factor=5;
 
 
 int preview_alpha=35;
 
-bool preview=false;
-bool image_only=false;
-void render(){
-	if(image_only){
-		cv::imshow(window_name,image);
-	}else{
-		cv::Mat i1;
-		image.copyTo(i1);
-		grid.drawBox(i1);
-		cv::imshow(window_name,i1);
+int disp_mode=3,n_mode=7;
+
+cv::Mat hist=cv::Mat::zeros(700,12,CV_8UC3);
+int hist_row=0;
+
+int constexpr sign(int x){
+	return (x>0)-(x<0);
+}
+
+cv::Mat computeAvg(cv::Mat i1,int zoom_factor=5,int border=1){
+	if(i1.empty())
+		i1=grid.extractScreen(zoom_factor);
+	assert(i1.rows==zoom_factor*grid.getMaxA()&&i1.cols==zoom_factor*grid.getMaxB());
+	assert(i1.type()==CV_8UC3);
+
+	cv::Mat avg(grid.getMaxA(),grid.getMaxB(),CV_8UC3);
+	for(int a=0;a<grid.getMaxA();++a)
+	for(int b=0;b<grid.getMaxB();++b){
+		cv::Vec3i sum(0,0,0);
+		for(int x=border;x<zoom_factor-border;++x)
+		for(int y=border;y<zoom_factor-border;++y)
+			sum+=i1.at<cv::Vec3b>(a*zoom_factor+x,b*zoom_factor+y);
+
+		avg.at<cv::Vec3b>(a,b)=sum/((zoom_factor-2*border)*(zoom_factor-2*border));
 	}
+	return avg;
+}
+
+void render(){
+	cv::Mat i2;
+	image.copyTo(i2);
+	grid.drawBox(i2);
+	cv::imshow(window_name,i2);
 
 	cv::Mat i1=grid.extractScreen(zoom_factor);
-	if(!image_only){
-		cv::cvtColor(i1,i1,cv::COLOR_BGR2GRAY);
-		if(preview){
-			cv::adaptiveThreshold(i1,i1,255,cv::ADAPTIVE_THRESH_GAUSSIAN_C,
-					cv::THRESH_BINARY,zoom_factor*5,2);
-		}
+	int constexpr border=1;
+	switch(disp_mode){
+		case 0:
+			// nothing
+			break;
+		case 1:
+			// grid
+			grid.drawGrid(i1);
+			break;
+		case 2:
+			{
+			// border
+			for(int a=0;a<i1.rows;a+=zoom_factor)
+			for(int b=0;b<i1.cols;b+=zoom_factor)
+				for(int x=0;x<border;++x)
+				for(int y=border;y<zoom_factor;++y)
+					i1.at<cv::Vec3b>(a+x,b+y)=
+					i1.at<cv::Vec3b>(a+zoom_factor-1-y,b+x)=
+					i1.at<cv::Vec3b>(a+zoom_factor-1-x,b+zoom_factor-1-y)=
+					i1.at<cv::Vec3b>(a+y,b+zoom_factor-1-x)=
+					cv::Vec3b(0,0,0);
+			break;
+			}
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			{
+			// border & avg
+			cv::Mat avg=computeAvg(i1);
+
+			for(int a=0;a<i1.rows;a+=zoom_factor)
+			for(int b=0;b<i1.cols;b+=zoom_factor){
+				for(int x=0;x<border;++x)
+				for(int y=border;y<zoom_factor;++y)
+					i1.at<cv::Vec3b>(a+x,b+y)=
+					i1.at<cv::Vec3b>(a+zoom_factor-1-y,b+x)=
+					i1.at<cv::Vec3b>(a+zoom_factor-1-x,b+zoom_factor-1-y)=
+					i1.at<cv::Vec3b>(a+y,b+zoom_factor-1-x)=
+					cv::Vec3b(0,0,0);
+
+				auto avg_ab=avg.at<cv::Vec3b>(a/zoom_factor,b/zoom_factor);
+				for(int x=0;x<zoom_factor;++x)
+				for(int y=0;y<zoom_factor;++y)
+					i1.at<cv::Vec3b>(a+x,b+y)=avg_ab;
+
+				if(a==zoom_factor*10&&b<zoom_factor*12)
+					hist.at<cv::Vec3b>(hist_row,b/zoom_factor)=avg_ab;
+			}
+
+			if(disp_mode>=4)
+				cv::cvtColor(i1,i1,cv::COLOR_BGR2GRAY);
+
+			cv::Mat h1,h2;
+			switch(disp_mode){
+				case 3:
+					cv::resize(hist,h1,{},8,1,cv::INTER_NEAREST);
+					break;
+				case 4:
+					cv::cvtColor(hist,h2,cv::COLOR_BGR2GRAY);
+					cv::resize(h2,h1,{},8,1,cv::INTER_NEAREST);
+					break;
+				case 5:
+				case 6:
+					cv::cvtColor(hist,h1,cv::COLOR_BGR2GRAY);
+					h2.create(h1.rows,h1.cols,CV_8UC1);
+					for(int c=0;c<h1.cols;++c){
+						int last_r=-1,last_delta=0;
+						for(int r=0;r<h2.rows;++r){
+							int delta=h1.at<uint8_t>((r+4+h1.rows)%h1.rows,c)
+								-h1.at<uint8_t>((r-4)%h1.rows,c);
+							if(std::abs(delta)<20)
+								delta=0;
+
+							// eliminate lines adjacent to other, larger lines
+							// with the same sign
+							if(delta!=0){
+								if(last_r<0){
+									last_r=r;
+								}else{
+									if(sign(delta)==sign(last_delta)){
+										if(std::abs(delta)<std::abs(last_delta))
+											delta=0;
+										else{
+											h2.at<uint8_t>(last_r,c)=127;
+											last_r=r;
+											last_delta=delta;
+										}
+									}else{
+										last_r=r;
+										last_delta=delta;
+									}
+								}
+							}
+
+							h2.at<uint8_t>(r,c)=std::min(255,std::max(0,delta+127));
+						}
+
+						// accumulated sum -> binarized image
+						if(disp_mode==6){
+							int cur=0;
+							for(int r=0;r<h2.rows;++r){
+								if(h2.at<uint8_t>(r,c)>127){
+									cur=1;
+								}else if(h2.at<uint8_t>(r,c)<127){
+									if(cur==0){
+										for(int r1=r;r1-->0;)
+											h2.at<uint8_t>(r1,c)=255;
+									}
+									cur=0;
+								}
+								h2.at<uint8_t>(r,c)=255*cur;
+							}
+						}
+					}
+					cv::resize(h2,h1,{},8,1,cv::INTER_NEAREST);
+					break;
+			}
+			cv::imshow(histwin_name,h1);
+			break;
+			}
 	}
+
 	cv::imshow(outwin_name,i1);
 }
 
@@ -136,6 +275,7 @@ int main(int argc,char** argv){
 
 	cv::namedWindow(window_name,cv::WINDOW_AUTOSIZE);
 	cv::namedWindow(outwin_name,cv::WINDOW_AUTOSIZE);
+	cv::namedWindow(histwin_name,cv::WINDOW_AUTOSIZE);
 
 	bool save_config;
 	{
@@ -219,27 +359,15 @@ move_corner:
 				render();
 				break;
 
-				/*
-			case '9':
-				grid.changeEdgeThreshold(-1);
+			case '1':
+				/// Change display mode
+				disp_mode=(disp_mode+n_mode-1)%n_mode;
 				render();
 				break;
 
-			case '0':
-				grid.changeEdgeThreshold(1);
-				render();
-				break;
-				*/
-
-			case 't':
-				/// Show only image - toggle
-				image_only^=true;
-				render();
-				break;
-
-			case 'p':
-				/// Preview (average color) - toggle
-				preview^=true;
+			case '2':
+				/// Change display mode
+				disp_mode=(disp_mode+1)%n_mode;
 				render();
 				break;
 
@@ -269,16 +397,37 @@ change_pixel:
 			case 'o':
 				/// Output
 			{
-				std::ofstream out("out.txt",std::ios::binary);
+				std::ofstream out("time.txt",std::ios::binary);
+				double fps=capture.get(cv::CAP_PROP_FPS);
 				while(capture.read(image)){
-					cv::cvtColor(image,image,cv::COLOR_BGR2GRAY);
+					if(fps!=capture.get(cv::CAP_PROP_FPS)){
+						std::cerr<<"variable fps: "<<fps<<"!="<<
+							capture.get(cv::CAP_PROP_FPS)<<'\n';
+						assert(false);
+					}
+
+					/* print position in nanosecond as 8-byte little-endian int
+					auto ns=int64_t(capture.get(cv::CAP_PROP_POS_MSEC)*1e6);
+					for(int i=0;i<8;++i,ns>>=8)
+						out<<(unsigned char)ns;
+						*/
+
 					grid.setImage(image);
-					cv::Mat screen=grid.extractScreen(1);
-					assert(screen.rows==grid.getMaxA());
-					assert(screen.cols==grid.getMaxB());
-					for(int a=0;a<screen.rows;++a)
-					for(int b=0;b<screen.cols;++b)
-						out<<(unsigned char)screen.at<uint8_t>(a,b);
+					cv::Mat avg=computeAvg({});
+					cv::cvtColor(avg,avg,cv::COLOR_BGR2GRAY);
+					assert(avg.rows==grid.getMaxA());
+					assert(avg.cols==grid.getMaxB());
+					assert(avg.type()==CV_8U);
+					
+					cv::Mat i1;
+					cv::resize(avg,i1,{},5,5,cv::INTER_NEAREST);
+					cv::imshow(outwin_name,i1);
+					cv::waitKey(1);
+
+					for(int a=0;a<avg.rows;++a)
+					for(int b=0;b<avg.cols;++b){
+						out<<(unsigned char)avg.at<uint8_t>(a,b);
+					}
 				}
 				out.close();
 				goto break_outer;
@@ -311,8 +460,21 @@ change_pixel:
 					std::cout<<"End of file\n";
 					break;
 				}
+				hist_row=(hist_row+1)%hist.rows;
 				grid.setImage(image);
 				render();
+				break;
+
+			case 'N':
+				for(int _=100;_-->0;){
+					if(!capture.read(image)){
+						std::cout<<"End of file\n";
+						break;
+					}
+					hist_row=(hist_row+1)%hist.rows;
+					grid.setImage(image);
+					render();
+				}
 				break;
 		}
 	}
